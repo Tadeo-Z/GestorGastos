091bd848c7e2cc056535484ade36beb243314e42
VegaSamuel <125225079+VegaSamuel@users.noreply.github.com>
2025-05-18 00:33:32 -0700

    Todas las rutas con autenticación

--- routes/countRoute.js
+++ routes/countRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const countController = require('../controllers/countController');
-const auth = require('../util/auth');
+const { auth } = require('../util/auth');
 
-router.get('/', countController.getCounts);
-router.get('/:id', countController.getCount);
-router.post('/', countController.addCount);
-router.put('/:id', countController.updateCount);
-router.delete('/:id', countController.deleteCount);
+router.get('/', auth, countController.getCounts);
+router.get('/:id', auth, countController.getCount);
+router.post('/', auth, countController.addCount);
+router.put('/:id', auth, countController.updateCount);
+router.delete('/:id', auth, countController.deleteCount);
 
 module.exports = router;
--- routes/groupRoute.js
+++ routes/groupRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const groupController = require('../controllers/groupController');
-const auth = require('../util/auth');
+const { auth } = require('../util/auth');
 
-router.get('/', groupController.getGroups);
-router.get('/:id', groupController.getGroup);
-router.post('/', groupController.addGroup);
-router.put('/:id', groupController.updateGroup);
-router.delete('/:id', groupController.deleteGroup);
+router.get('/', auth, groupController.getGroups);
+router.get('/:id', auth, groupController.getGroup);
+router.post('/', auth, groupController.addGroup);
+router.put('/:id', auth, groupController.updateGroup);
+router.delete('/:id', auth, groupController.deleteGroup);
 
 module.exports = router;
